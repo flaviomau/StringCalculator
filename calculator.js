a43f151675179f3c2d6b4b7d836ffa10772a7e19
flaviomau <flaviomau@yahoo.com>
2019-05-15 15:33:42 -0300

    Stage 1 cleared.

--- calculator.js
+++ calculator.js
@@ -1,5 +1,15 @@
-function add(numbers){
-    return 0
+function add(numbers) {
+    let separator = ','
+
+    if (!numbers || numbers.length == 0)
+        return 0
+
+    const values = numbers.split(separator)
+    const result = values.reduce((acc, value) => {
+        return parseInt(acc) + parseInt(value)
+    })
+
+    return parseInt(result)
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -2,9 +2,21 @@ const assert = require('assert')
 const {add} = require('./calculator')
 
 describe('String Calculator tests', function(){
-    it('sum 2+2', function(){
-        const expected = 4
-        const result = add('2,2')
-        assert.equal(expected, result)
+    it('sum 1,2,5', function(){
+        const expected = 8
+        const result = add('1,2,5')
+        assert.deepEqual(expected, result)
+    })
+
+    it('sum empty array', function(){
+        const expected = 0
+        const result = add('')
+        assert.deepEqual(expected, result)
+    })
+
+    it('sum null array', function(){
+        const expected = 0
+        const result = add(null)
+        assert.deepEqual(expected, result)
     })
 })
